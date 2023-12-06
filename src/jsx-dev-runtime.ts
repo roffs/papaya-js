6ef01e6e56f2a1afd1cc1c8fdad535e2b3a4f8b3
Gerard Fabregó <gerardfabrego@gmail.com>
2023-12-06 22:10:23 +0100

    Format jsx-dev-runtime

--- src/jsx-dev-runtime.ts
+++ src/jsx-dev-runtime.ts
@@ -3,32 +3,26 @@
 
 import { Element, ElementType, Props } from "./types";
 
-
 export function jsxDEV(type: ElementType, props?: Props | null): Element {
-
-    console.log("Type", type)
-    console.log("Props", props)
-    console.log("Children", props?.children)
-
-    return {
-        type,
-        props: {
-            ...props,
-            children: typeof props?.children === 'string'
-                ? [createTextElement(props?.children)]
-                : props?.children
-        }
-    }
+  return {
+    type,
+    props: {
+      ...props,
+      children:
+        typeof props?.children === "string"
+          ? [createTextElement(props?.children)]
+          : props!.children,
+    },
+  };
 }
 
-
 function createTextElement(text: string): Element {
-    const textElement: Element = {
-        type: ElementType.TEXT,
-        props: {
-            "nodeValue": text,
-            children: [],
-        },
-    }
-    return textElement;
+  const textElement: Element = {
+    type: ElementType.TEXT,
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+  return textElement;
 }
