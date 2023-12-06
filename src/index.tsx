8eb2e16bc51b4aceffb3beb2390b8e6ba7b85317
Gerard Fabregó <gerardfabrego@gmail.com>
2023-12-06 22:07:49 +0100

    Format with prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,132 +1,108 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
-import { Element, ElementType, Props } from "./types";
-
+import { Element, Fiber, Nullable } from "./types";
 
 function createDomElement(fiber: Fiber): HTMLElement {
-    const dom = document.createElement(fiber.type);
+  const dom = document.createElement(fiber.type);
 
-    Object.keys(fiber.props)
-        .filter(key => key !== "children")
-        .forEach(key => {
-            key == 'nodeValue'
-                ? dom.textContent = fiber.props[key]
-                : dom.setAttribute(key, fiber.props[key])
+  Object.keys(fiber.props)
+    .filter((key) => key !== "children")
+    .forEach((key) => {
+      key == "nodeValue"
+        ? (dom.textContent = fiber.props[key])
+        : dom.setAttribute(key, fiber.props[key]);
+    });
 
-        })
-
-    // ????
-    // fiber.props.children?.forEach(child =>
-    //     render(child, dom)
-    // )
-
-    return dom;
+  return dom;
 }
 
-let nextUnitOfWork: Nullable<Fiber> = null
-let wipRoot: Nullable<Fiber> = null
-
-
-
-const element: Element = (
-    <div id="foo" >
-        <a>bar </a>
-        < b />
-    </div>
-)
-
-type Nullable<T> = T | null;
-
-type Fiber = {
-    type: ElementType,
-    props: Props,
-    parent?: Fiber,
-    sibling?: Fiber,
-    child?: Fiber,
-    dom: Nullable<HTMLElement>,
-}
+let nextUnitOfWork: Nullable<Fiber> = null;
+let wipRoot: Nullable<Fiber> = null;
 
 function commitRoot() {
-    commitWork(wipRoot!.child)
-    wipRoot = null
+  commitWork(wipRoot!.child);
+  wipRoot = null;
 }
 
 function commitWork(fiber) {
-    if (!fiber) {
-        return
-    }
-    const domParent = fiber.parent.dom
-    domParent.appendChild(fiber.dom)
-    commitWork(fiber.child)
-    commitWork(fiber.sibling)
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
 }
 
 function workLoop(deadline) {
-    let shouldYield = false
-    while (nextUnitOfWork && !shouldYield) {
-        nextUnitOfWork = performUnitOfWork(
-            nextUnitOfWork
-        )
-        shouldYield = deadline.timeRemaining() < 1
-    }
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
 
-    if (!nextUnitOfWork && wipRoot) {
-        commitRoot()
-    }
-    requestIdleCallback(workLoop)
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+  requestIdleCallback(workLoop);
 }
 
-requestIdleCallback(workLoop)
-
+requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber: Fiber): Nullable<Fiber> {
-    if (!fiber.dom) {
-        fiber.dom = createDomElement(fiber)
+  if (!fiber.dom) {
+    fiber.dom = createDomElement(fiber);
+  }
+
+  const elements = fiber.props.children;
+
+  let prevSibling: Fiber;
+  elements
+    .map((element) => ({
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }))
+    .forEach((newFiber: Fiber, index) => {
+      prevSibling = newFiber;
+      index === 0 ? (fiber.child = newFiber) : (prevSibling.sibling = newFiber);
+    });
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber: Fiber = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
     }
+    nextFiber = nextFiber.parent!;
+  }
 
-    const elements = fiber.props.children
-
-    let prevSibling: Fiber;
-    elements.map(element => ({
-        type: element.type,
-        props: element.props,
-        parent: fiber,
-        dom: null,
-    })).forEach((newFiber: Fiber, index) => {
-        prevSibling = newFiber // Va aqui o a sota? 
-        index === 0
-            ? fiber.child = newFiber
-            : prevSibling.sibling = newFiber
-    })
-
-
-    if (fiber.child) {
-        return fiber.child
-    }
-
-    let nextFiber: Fiber = fiber
-
-    while (nextFiber) {
-        if (nextFiber.sibling) {
-            return nextFiber.sibling
-        }
-        nextFiber = nextFiber.parent!
-    }
-
-    return null;
+  return null;
 }
 
 function render(element: Element, container: HTMLElement) {
-    wipRoot = {
-        type: element.type,
-        dom: container,
-        props: {
-            children: [element],
-        },
-    }
-    nextUnitOfWork = wipRoot;
+  wipRoot = {
+    type: element.type,
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+  nextUnitOfWork = wipRoot;
 }
 
-const container = document.getElementById("root")!
+const element: Element = (
+  <div id="foo">
+    <a>bar </a>
+    <b />
+  </div>
+);
+
+const container = document.getElementById("root")!;
 render(element, container);
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,27 @@
 export type Element = {
-    type: ElementType,
-    props: Props,
-}
+  type: ElementType;
+  props: Props;
+};
 
 export enum ElementType {
-    TEXT = 'text',
-    DIV = 'div',
-    A = 'a',
-    B = 'b'
+  TEXT = "text",
+  DIV = "div",
+  A = "a",
+  B = "b",
 }
 
 export type Props = {
-    children: Element[],
-    [key: string]: any
-}
+  children: Element[];
+  [key: string]: any;
+};
+
+export type Nullable<T> = T | null;
+
+export type Fiber = {
+  type: ElementType;
+  props: Props;
+  parent?: Fiber;
+  sibling?: Fiber;
+  child?: Fiber;
+  dom: Nullable<HTMLElement>;
+};
